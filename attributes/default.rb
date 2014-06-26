c72090ac55e34d77f07610603bfd4e30d0f7845c
Barry Hoggard <barry@paddle8.com>
2014-06-26 11:42:59 -0400

    first pass for vagrant testing

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default[:opsworks_env][:dir] = "/etc/profile.d"
+default[:opsworks_env][:json_key] = "custom_env"
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             "opsworks_env"
+maintainer       "Barry Hoggard"
+maintainer_email "barry@tristanmedia.com"
+license          "MIT"
+description      "Chef cookbook to write out /etc/profile.d file based on and OpsWorks stack's custom JSON"
+version          "0.1"
