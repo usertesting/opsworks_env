97896b7b344bd49449f14aa3ab92b119a85cd9b8
Barry Hoggard <barry@paddle8.com>
2014-06-26 11:52:12 -0400

    non-opsworks testing first

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,4 @@
-node[:deploy].each do |application, deploy|
+# node[:deploy].each do |application, deploy|
   template "custom_env.sh" do
     path "#{node[:custom_env][:dir]}/custom_env.sh"
     source "custom_env.sh.erb"
@@ -9,4 +9,4 @@ node[:deploy].each do |application, deploy|
     # variables(:env => node[:custom_env][application])
     # notifies :restart, resources(:service => "redis")
   end
-end
+# end
