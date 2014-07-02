f37b3728af7f53cc79f5bb687e1253ab39bcac1a
Barry Hoggard <barry@paddle8.com>
2014-07-02 15:29:32 -0400

    Added a script to export vars before a rails restart

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |application, deploy|
   execute "restart Rails app #{application}" do
     if node[:opsworks].has_key?(:rails_stack)
       cwd deploy[:current_path]
-      command "source /etc/profile.d/custom_env.sh; #{node[:opsworks][:rails_stack][:restart_command]}"
+      command "source /usr/bin/use_custom_env.sh; #{node[:opsworks][:rails_stack][:restart_command]}"
       action :nothing
     end
   end
@@ -10,6 +10,16 @@ node[:deploy].each do |application, deploy|
   template 'custom_env.sh' do
     path   '/etc/profile.d/custom_env.sh'
     source 'custom_env.sh.erb'
+    group  'root'
+    owner  'root'
+    mode   '0755'
+    variables(:env => node[:custom_env][application])
+    notifies :run, resources(:execute => "restart Rails app #{application}")
+  end
+
+  template 'use_custom_env.sh' do
+    path   '/usr/local/bin'
+    source 'user_custom_env.sh.erb'
     group  'root'
     owner  'root'
     mode   '0755'
